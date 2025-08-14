126b7dc3a2e36359f93d2a07ffb8ce4204979244
aryancsgupta <aryangupta1467@gmail.com>
2025-08-14 15:33:03 +0530

    14-08-2025

---
+++ Level1/PenDistribution.java
@@ -0,0 +1,12 @@
+public class PenDistribution {
+    public static void main(String[] args) {
+        int totalPens = 14;
+        int totalStudents = 3;
+
+        int pensPerStudent = totalPens / totalStudents; // division operator
+        int remainingPens = totalPens % totalStudents;  // modulus operator
+
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                           " and the remaining pen not distributed is " + remainingPens);
+    }
+}
