b3d655a13378c2948888b7b19d51509662ad2112
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2025-08-28 17:37:51 +0530

    [Aryan] Add : Number format demo problem

---
+++ String/Level_1/NumberFormatDemo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumberFormatDemo {
+    public static void generateException(String text) {
+        int number = Integer.parseInt(text);
+        System.out.println("Converted number: " + number);
+    }
+
+    public static void handleException(String text) {
+        try {
+            int number = Integer.parseInt(text);
+            System.out.println("Converted number: " + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Caught NumberFormatException: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        
+        handleException(text);
+    }
+}
